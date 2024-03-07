2e62fca1f18f65a8b2934a94dd7188661e9b6152
wsant <wsant@Trueno>
2024-03-06 21:34:48 -0500

    Se sube primera version

---
+++ src/main/java/LowLevelDesign/DesignVendingMachine/UsernamePasswordAuthentication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LowLevelDesign.DesignVendingMachine;
+
+/**
+ *
+ * @author johan
+ */
+public class UsernamePasswordAuthentication implements AuthenticationMethod {
+    public void authenticate() {
+        // Lógica para autenticar con usuario y contraseña...
+    }
+}
+
